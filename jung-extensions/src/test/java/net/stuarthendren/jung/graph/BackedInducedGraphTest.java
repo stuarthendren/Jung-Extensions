0df08beb28d35e6211c08fa04535723042fc9987
Stuart Hendren <stuarthendren@gmail.com>
2012-04-16 21:45:57 +0100

    Adding tests for static and backed induced graph

--- jung-extensions/src/test/java/net/stuarthendren/jung/graph/BackedInducedGraphTest.java
+++ jung-extensions/src/test/java/net/stuarthendren/jung/graph/BackedInducedGraphTest.java
@@ -7,17 +7,18 @@ import org.junit.Test;
 import edu.uci.ics.jung.graph.Graph;
 import static junit.framework.Assert.assertEquals;
 
-public class StaticInducedGraphTest extends AbstractInducedGraphTest {
+public class BackedInducedGraphTest extends AbstractInducedGraphTest {
 
 	@Override
 	public Graph<Integer, Integer> createInducedGraph(Graph<Integer, Integer> base, Set<Integer> vertices) {
-		return new StaticInducedGraph<Integer, Integer>(base, vertices);
+		return new BackedInducedGraph<Integer, Integer>(base, vertices);
 	}
 
 	@Test
-	public void testStatic() {
+	public void testDynamic() {
 		int vertexCount1 = graph.getVertexCount();
 		vertices.add(4);
-		assertEquals(vertexCount1, graph.getVertexCount());
+		assertEquals(vertexCount1 + 1, graph.getVertexCount());
+		assertEquals(6, graph.getEdgeCount());
 	}
 }
