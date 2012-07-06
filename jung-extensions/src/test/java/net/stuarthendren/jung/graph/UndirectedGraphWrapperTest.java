a58387f3b84e6de2da5f1ce969bfec655117af0b
Stuart Hendren <stuarthendren@gmail.com>
2012-07-06 22:15:07 +0100

    Adding UndirectedGraphWrapperTest

---
+++ jung-extensions/src/test/java/net/stuarthendren/jung/graph/UndirectedGraphWrapperTest.java
@@ -0,0 +1,40 @@
+package net.stuarthendren.jung.graph;
+
+import org.junit.Test;
+
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.UndirectedGraph;
+import edu.uci.ics.jung.graph.util.EdgeType;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SuppressWarnings("unchecked")
+public class UndirectedGraphWrapperTest {
+
+	@Test
+	public void testUndirectedGraphWrapperGraph() {
+		Graph<Integer, Integer> graph = mock(Graph.class);
+		UndirectedGraphWrapper<Integer, Integer> undirectedGraphWrapper = new UndirectedGraphWrapper<Integer, Integer>(
+				graph);
+		assertTrue(undirectedGraphWrapper instanceof UndirectedGraph);
+	}
+
+	@Test
+	public void testUndirectedGraphWrapperWithCheck() {
+		Graph<Integer, Integer> graph = mock(Graph.class);
+		UndirectedGraphWrapper<Integer, Integer> undirectedGraphWrapper = new UndirectedGraphWrapper<Integer, Integer>(
+				graph, true);
+		assertTrue(undirectedGraphWrapper instanceof UndirectedGraph);
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testUndirectedGraphWrapperWithCheckThrowsException() {
+		Graph<Integer, Integer> graph = mock(Graph.class);
+		when(graph.getEdgeCount(EdgeType.DIRECTED)).thenReturn(10);
+		UndirectedGraphWrapper<Integer, Integer> undirectedGraphWrapper = new UndirectedGraphWrapper<Integer, Integer>(
+				graph, true);
+	}
+
+}
